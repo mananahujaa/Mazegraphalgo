3ff0d1be964f1379e36f57a8e24f382d452f686b
mananahujaa <63778460+mananahujaa@users.noreply.github.com>
2023-12-01 18:54:30 -0500

    boost cpp

--- main.cpp
+++ main.cpp
@@ -1,89 +1,227 @@
+// #include <iostream>
+// #include <fstream>
+// #include <vector>
+// #include <set>
+// #include <boost/graph/adjacency_list.hpp>
+// using namespace std;
+// using namespace boost;
+
+// // Define the graph type
+// typedef adjacency_list<vecS, vecS, directedS> Graph;
+// typedef graph_traits<Graph>::vertex_descriptor Vertex;
+
+// // A utility function to add an edge in the graph
+// void add_edge_if_valid(Graph& g, int from, int to, int rows, int cols) {
+//     // Add your logic here to add an edge if it's valid within the maze
+// }
+
+// // DFS function with backtracking
+// bool dfs(Graph& g, Vertex v, Vertex target, set<Vertex>& visited, vector<Vertex>& path) {
+//     if (v == target) {
+//         path.push_back(v);
+//         return true;
+//     }
+//     if (visited.find(v) != visited.end()) {
+//         return false;
+//     }
+    
+//     visited.insert(v);
+//     graph_traits<Graph>::adjacency_iterator ai, ai_end;
+//     for (tie(ai, ai_end) = adjacent_vertices(v, g); ai != ai_end; ++ai) {
+//         if (dfs(g, *ai, target, visited, path)) {
+//             path.push_back(v);
+//             return true;
+//         }
+//     }
+//     visited.erase(v);
+//     return false;
+// }
+
+
+// int main() {
+//     ifstream infile("input.txt");
+//     if (!infile) {
+//         cerr << "Error opening input file" << endl;
+//         return 1;
+//     }
+
+//     int rows, cols;
+//     infile >> rows >> cols;
+
+//     // Create the graph
+//     Graph g(rows * cols);
+
+//     // Read the maze and construct the graph
+//     for (int i = 0; i < rows; ++i) {
+//         for (int j = 0; j < cols; ++j) {
+//             // Read and process each cell to create edges
+//             // Use add_edge_if_valid to add edges to the graph
+//         }
+//     }
+
+//     // Define the start and target vertices
+//     Vertex start = 0; // Assuming top-left corner is the start
+//     Vertex target = rows * cols - 1; // Assuming bottom-right corner is the target
+
+//     set<Vertex> visited;
+//     vector<Vertex> path;
+//     if (dfs(g, start, target, visited, path)) {
+//         reverse(path.begin(), path.end());
+//         // Output the path
+//         ofstream outfile("output.txt");
+//         for (Vertex v : path) {
+//             outfile << /* format the output according to the problem's requirements */;
+//         }
+//     } else {
+//         cout << "No solution found" << endl;
+//     }
+
+//     return 0;
+//}
+
+
+
+
+
+
+
 #include <iostream>
-#include <fstream>
 #include <vector>
 #include <set>
-#include <boost/graph/adjacency_list.hpp>
+#include <algorithm>
+
 using namespace std;
-using namespace boost;
 
-// Define the graph type
-typedef adjacency_list<vecS, vecS, directedS> Graph;
-typedef graph_traits<Graph>::vertex_descriptor Vertex;
+// Define Move structure
+struct Move {
+    int row;
+    int col;
 
-// A utility function to add an edge in the graph
-void add_edge_if_valid(Graph& g, int from, int to, int rows, int cols) {
-    // Add your logic here to add an edge if it's valid within the maze
+    Move(int r, int c) : row(r), col(c) {}
+};
+
+enum Direction {
+    UP = 0,
+    DOWN = 1,
+    LEFT = 2,
+    RIGHT = 3
+};
+
+string getDirectionString(int direction) {
+    switch (direction) {
+        case UP: return "S";
+        case DOWN: return "N";
+        case LEFT: return "W";
+        case RIGHT: return "E";
+        default: return "";
+    }
 }
 
-// DFS function with backtracking
-bool dfs(Graph& g, Vertex v, Vertex target, set<Vertex>& visited, vector<Vertex>& path) {
-    if (v == target) {
-        path.push_back(v);
-        return true;
+// Define MazeSolver class
+class MazeSolver {
+public:
+    MazeSolver(const vector<vector<string>>& mazeGraph) : graph(mazeGraph) {}
+
+    vector<Move> solveMaze(const pair<int, int>& startNode, const pair<int, int>& targetNode) {
+        path.clear();
+        visited.clear();
+
+        if (dfs(startNode, true, targetNode)) {
+            reverse(path.begin(), path.end());
+            return path;
+        } else {
+            cerr << "No solution found\n";
+            return vector<Move>();
+        }
     }
-    if (visited.find(v) != visited.end()) {
-        return false;
-    }
-    
-    visited.insert(v);
-    graph_traits<Graph>::adjacency_iterator ai, ai_end;
-    for (tie(ai, ai_end) = adjacent_vertices(v, g); ai != ai_end; ++ai) {
-        if (dfs(g, *ai, target, visited, path)) {
-            path.push_back(v);
+
+private:
+    const vector<vector<string>>& graph;
+    vector<Move> path;
+    set<pair<int, int>> visited;
+
+    bool dfs(const pair<int, int>& currentNode, bool isRedArrow, const pair<int, int>& targetNode) {
+        if (currentNode == targetNode) {
             return true;
         }
-    }
-    visited.erase(v);
-    return false;
-}
 
+        if (visited.count(currentNode) > 0) {
+            return false;
+        }
+
+        visited.insert(currentNode);
+        vector<pair<int, int>> neighbors = getNeighbors(currentNode);
+
+        for (const auto& neighbor : neighbors) {
+            if (dfs(neighbor, !isRedArrow, targetNode)) {
+                Move moveInstance{neighbor.first, neighbor.second};
+                path.push_back(moveInstance);
+                return true;
+            }
+        }
+
+        visited.erase(currentNode);
+        return false;
+    }
+
+    vector<pair<int, int>> getNeighbors(const pair<int, int>& currentNode) {
+        int row = currentNode.first;
+        int col = currentNode.second;
+
+        vector<pair<int, int>> neighbors;
+
+        // Move up
+        if (row > 0) {
+            neighbors.push_back({row - 1, col});
+        }
+
+        // Move down
+        if (row < graph.size() - 1) {
+            neighbors.push_back({row + 1, col});
+        }
+
+        // Move left
+        if (col > 0) {
+            neighbors.push_back({row, col - 1});
+        }
+
+        // Move right
+        if (col < graph[0].size() - 1) {
+            neighbors.push_back({row, col + 1});
+        }
+
+        return neighbors;
+    }
+};
 
 int main() {
-    ifstream infile("input.txt");
-    if (!infile) {
-        cerr << "Error opening input file" << endl;
-        return 1;
+    // Example maze graph
+    vector<vector<string>> mazeGraph = {
+        {"R-S", "R-S", "R-SE"},
+        {"B-NE", "B-NW", "R-S"},
+        {"B-NE", "B-E", "O"}
+    };
+
+    // Create MazeSolver instance
+    MazeSolver mazeSolver(mazeGraph);
+
+    // Set start and target nodes
+    pair<int, int> startNode = {0, 0};
+    pair<int, int> targetNode = {2, 2};
+
+    // Solve the maze
+    vector<Move> path = mazeSolver.solveMaze(startNode, targetNode);
+
+    // Display the result
+    for (size_t i = 0; i < path.size(); ++i) {
+        string direction = getDirectionString(i % 4);
+        int steps = (i / 4) + 1;
+        cout << steps << direction << " ";
     }
-
-    int rows, cols;
-    infile >> rows >> cols;
-
-    // Create the graph
-    Graph g(rows * cols);
-
-    // Read the maze and construct the graph
-    for (int i = 0; i < rows; ++i) {
-        for (int j = 0; j < cols; ++j) {
-            // Read and process each cell to create edges
-            // Use add_edge_if_valid to add edges to the graph
-        }
-    }
-
-    // Define the start and target vertices
-    Vertex start = 0; // Assuming top-left corner is the start
-    Vertex target = rows * cols - 1; // Assuming bottom-right corner is the target
-
-    set<Vertex> visited;
-    vector<Vertex> path;
-    if (dfs(g, start, target, visited, path)) {
-        reverse(path.begin(), path.end());
-        // Output the path
-        ofstream outfile("output.txt");
-        for (Vertex v : path) {
-            outfile << /* format the output according to the problem's requirements */;
-        }
-    } else {
-        cout << "No solution found" << endl;
-    }
+    cout << endl;
 
     return 0;
 }
-
-
-
-
-
-
 
 
 
